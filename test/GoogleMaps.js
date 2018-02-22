969d1626dddf433519d8b63d1b978aef1099b2b3
Jeff Andrews <jeff.andrews@carpages.ca>
2018-02-22 13:40:51 -0500

    Add support for custom google maps library version. Falls back to default version.

--- test/GoogleMaps.js
+++ test/GoogleMaps.js
@@ -1,108 +1,180 @@
-(function() {
+( function() {
+  var baseUrl = GoogleMapsLoader.URL;
 
-	var baseUrl = GoogleMapsLoader.URL;
+  describe( 'GoogleMaps', function() {
+    beforeEach( function() {
+      GoogleMapsLoader.makeMock();
+    });
 
-	var cb = GoogleMapsLoader.WINDOW_CALLBACK_NAME;
+    afterEach( function( done ) {
+      GoogleMapsLoader.release( function() {
+        done();
+      });
+    });
 
-	describe('GoogleMaps', function() {
+    describe( 'defaults', function() {
+      it( 'should have a default version', function() {
+        expect( GoogleMapsLoader.defaultOptions.v ).to.be.equal(
+          GoogleMapsLoader.VERSION
+        );
+      });
 
-		beforeEach(function() {
-			GoogleMapsLoader.makeMock();
-		});
+      it( 'should have a default callback name', function() {
+        expect( GoogleMapsLoader.defaultOptions.callback ).to.be.equal(
+          GoogleMapsLoader.WINDOW_CALLBACK_NAME
+        );
+      });
+    });
 
-		afterEach(function(done) {
-			GoogleMapsLoader.release(function() {
-				done();
-			});
-		});
+    describe( '#load', function() {
+      it( 'should load google api object', function( done ) {
+        GoogleMapsLoader.load( function( google ) {
+          expect( google ).to.be.a( 'object' );
+          expect( GoogleMapsLoader.isLoaded()).to.be.true;
+          done();
+        });
+      });
 
-		describe('#load', function() {
+      it( 'should load google api only for first time and then use stored object', function( done ) {
+        var count = 0;
 
-			it('should load google api object', function(done) {
-				GoogleMapsLoader.load(function(google) {
-					expect(google).to.be.a('object');
-					expect(GoogleMapsLoader.isLoaded()).to.be.true;
-					done();
-				});
-			});
+        GoogleMapsLoader.onLoad( function() {
+          count++;
+        });
 
-			it('should load google api only for first time and then use stored object', function(done) {
-				var count = 0;
+        GoogleMapsLoader.load();
+        GoogleMapsLoader.load();
+        GoogleMapsLoader.load();
 
-				GoogleMapsLoader.onLoad(function() {
-					count++;
-				});
+        GoogleMapsLoader.load( function() {
+          expect( count ).to.be.equal( 1 );
+          done();
+        });
+      });
+    });
 
-				GoogleMapsLoader.load();
-				GoogleMapsLoader.load();
-				GoogleMapsLoader.load();
+    describe( '#release', function() {
+      it( 'should restore google maps package to original state and remove google api object completely and load it again', function( done ) {
+        GoogleMapsLoader.load( function() {
+          expect( GoogleMapsLoader.isLoaded()).to.be.true;
 
-				GoogleMapsLoader.load(function() {
-					expect(count).to.be.equal(1);
-					done()
-				});
-			});
-		});
+          GoogleMapsLoader.release( function() {
+            expect( GoogleMapsLoader.isLoaded()).to.be.false;
 
-		describe('#release', function() {
+            GoogleMapsLoader.makeMock();
+            GoogleMapsLoader.load( function() {
+              expect( GoogleMapsLoader.isLoaded()).to.be.true;
+              done();
+            });
+          });
+        });
+      });
+    });
 
-			it('should restore google maps package to original state and remove google api object completely and load it again', function(done) {
-				GoogleMapsLoader.load(function() {
-					expect(GoogleMapsLoader.isLoaded()).to.be.true;
+    describe( '#createUrl', function() {
+      it( 'should create url with key', function() {
+        var expectedOptions = JSON.parse(
+          JSON.stringify( GoogleMapsLoader.defaultOptions )
+        );
+        expectedOptions.key = 'abcdefghijkl';
 
-					GoogleMapsLoader.release(function() {
-						expect(GoogleMapsLoader.isLoaded()).to.be.false;
+        GoogleMapsLoader.KEY = 'abcdefghijkl';
 
-						GoogleMapsLoader.makeMock();
-						GoogleMapsLoader.load(function() {
-							expect(GoogleMapsLoader.isLoaded()).to.be.true;
-							done();
-						});
-					});
-				});
-			});
+        expect( GoogleMapsLoader.createUrl()).to.be.equal(
+          baseUrl + GoogleMapsLoader._parseOptions( expectedOptions )
+        );
+      });
 
-		});
+      it( 'should create url with one library', function() {
+        var expectedOptions = JSON.parse(
+          JSON.stringify( GoogleMapsLoader.defaultOptions )
+        );
+        expectedOptions.libraries = [ 'hello' ];
 
-		describe('#createUrl', function() {
+        GoogleMapsLoader.LIBRARIES = [ 'hello' ];
 
-			it('should create url with key', function() {
-				GoogleMapsLoader.KEY = 'abcdefghijkl';
-				expect(GoogleMapsLoader.createUrl()).to.be.equal(baseUrl + '?callback=' + cb + '&key=abcdefghijkl');
-			});
+        expect( GoogleMapsLoader.createUrl()).to.be.equal(
+          baseUrl + GoogleMapsLoader._parseOptions( expectedOptions )
+        );
+      });
 
-			it('should create url with one library', function() {
-				GoogleMapsLoader.LIBRARIES = ['hello'];
-				expect(GoogleMapsLoader.createUrl()).to.be.equal(baseUrl + '?callback=' + cb + '&libraries=hello');
-			});
+      it( 'should create url with more libraries', function() {
+        var expectedOptions = JSON.parse(
+          JSON.stringify( GoogleMapsLoader.defaultOptions )
+        );
+        expectedOptions.libraries = [ 'hello', 'day' ];
 
-			it('should create url with more libraries', function() {
-				GoogleMapsLoader.LIBRARIES = ['hello', 'day'];
-				expect(GoogleMapsLoader.createUrl()).to.be.equal(baseUrl + '?callback=' + cb + '&libraries=hello,day');
-			});
+        GoogleMapsLoader.LIBRARIES = [ 'hello', 'day' ];
 
-			it('should create url with client and version', function() {
-				GoogleMapsLoader.CLIENT = 'buf';
-				GoogleMapsLoader.VERSION = '999';
-				expect(GoogleMapsLoader.createUrl()).to.be.equal(baseUrl + '?callback=' + cb + '&client=buf&v=999');
-			});
+        expect( GoogleMapsLoader.createUrl()).to.be.equal(
+          baseUrl + GoogleMapsLoader._parseOptions( expectedOptions )
+        );
+      });
 
-			it('should create url with channel', function() {
-				GoogleMapsLoader.CHANNEL = 'abcdefghijkl';
-				expect(GoogleMapsLoader.createUrl()).to.be.equal(baseUrl + '?callback=' + cb + '&channel=abcdefghijkl');
-			});
+      it( 'should create url with client', function() {
+        var expectedOptions = JSON.parse(
+          JSON.stringify( GoogleMapsLoader.defaultOptions )
+        );
+        expectedOptions.client = 'buf';
 
-			it('should create url with language', function() {
-				GoogleMapsLoader.LANGUAGE = 'fr';
-				expect(GoogleMapsLoader.createUrl()).to.be.equal(baseUrl + '?callback=' + cb + '&language=fr');
-			});
+        GoogleMapsLoader.CLIENT = 'buf';
 
-			it('should create url with region', function() {
-				GoogleMapsLoader.REGION = 'GB';
-				expect(GoogleMapsLoader.createUrl()).to.be.equal(baseUrl + '?callback=' + cb + '&region=GB');
-			});
-		});
+        expect( GoogleMapsLoader.createUrl()).to.be.equal(
+          baseUrl + GoogleMapsLoader._parseOptions( expectedOptions )
+        );
+      });
 
-	});
+      it( 'should create url with a specific version', function() {
+        var expectedOptions = JSON.parse(
+          JSON.stringify( GoogleMapsLoader.defaultOptions )
+        );
+        expectedOptions.v = '3.30';
 
-}).call();
+        GoogleMapsLoader.VERSION = '3.30';
+
+        expect( GoogleMapsLoader.createUrl()).to.be.equal(
+          baseUrl + GoogleMapsLoader._parseOptions( expectedOptions )
+        );
+      });
+
+      it( 'should create url with channel', function() {
+        var expectedOptions = JSON.parse(
+          JSON.stringify( GoogleMapsLoader.defaultOptions )
+        );
+        expectedOptions.channel = 'abcdefghijkl';
+
+        GoogleMapsLoader.CHANNEL = 'abcdefghijkl';
+
+        expect( GoogleMapsLoader.createUrl()).to.be.equal(
+          baseUrl + GoogleMapsLoader._parseOptions( expectedOptions )
+        );
+      });
+
+      it( 'should create url with language', function() {
+        var expectedOptions = JSON.parse(
+          JSON.stringify( GoogleMapsLoader.defaultOptions )
+        );
+        expectedOptions.language = 'fr';
+
+        GoogleMapsLoader.LANGUAGE = 'fr';
+
+        expect( GoogleMapsLoader.createUrl()).to.be.equal(
+          baseUrl + GoogleMapsLoader._parseOptions( expectedOptions )
+        );
+      });
+
+      it( 'should create url with region', function() {
+        var expectedOptions = JSON.parse(
+          JSON.stringify( GoogleMapsLoader.defaultOptions )
+        );
+        expectedOptions.region = 'GB';
+
+        GoogleMapsLoader.REGION = 'GB';
+
+        expect( GoogleMapsLoader.createUrl()).to.be.equal(
+          baseUrl + GoogleMapsLoader._parseOptions( expectedOptions )
+        );
+      });
+    });
+  });
+}.call());
